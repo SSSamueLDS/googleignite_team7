a7ebd98e177dc7fea2574642c169ed224f1f61cb
samuel <samuel240303@gmail.com>
2023-10-01 20:05:04 +0800

    search bar in progress

--- googleignite_team7/lib/screens/buyer_landing.dart
+++ googleignite_team7/lib/screens/buyer_landing.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: override_on_non_overriding_member
+
 import 'package:flutter/material.dart';
 import 'package:googleignite_team7/widgets/MyFilledButton.dart';
 
@@ -14,7 +16,59 @@ class BuyerLanding extends StatelessWidget {
             fit: BoxFit.cover,
           )),
         ),
+        SingleChildScrollView(
+                  child: Column(
+                  children: [
+                    _buildLogo(context),  
+                    _buildSearchBar(context),
+                  ],
+                  )
+                ),
       ]),
     );
   }
+  @override
+  Widget _buildLogo(BuildContext context) {
+  return Container(
+    height: 200,
+    margin: const EdgeInsets.only(top: 30),
+    child: Image.asset(
+      'assets/images/logo_without_text.png',
+    ),
+  );
+}
+
+@override
+  Widget _buildSearchBar(BuildContext context) {
+    bool searchText = true;
+    return Form(
+      child: Column(
+        
+        children: [
+           TextFormField(
+            
+            obscureText: searchText,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              prefixIcon: IconButton(
+                onPressed: () {
+                  searchText = !searchText;
+                },
+                icon: Icon(
+                     Icons.search,
+                  color: Colors.black,
+                ),
+              ),
+              labelText: 'Search Store',
+              labelStyle: TextStyle(
+                color: Colors.grey,
+              ),
+              ),
+            ),
+        ],
+          
+      ),
+      );
+}
 }
