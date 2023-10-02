7cb10c20d671977ef1205d046b1d4257deb5e070
samuel <samuel240303@gmail.com>
2023-10-02 13:33:42 +0800

    searchbar widget done

--- googleignite_team7/lib/screens/buyer_landing.dart
+++ googleignite_team7/lib/screens/buyer_landing.dart
@@ -17,58 +17,73 @@ class BuyerLanding extends StatelessWidget {
           )),
         ),
         SingleChildScrollView(
-                  child: Column(
-                  children: [
-                    _buildLogo(context),  
-                    _buildSearchBar(context),
-                  ],
-                  )
-                ),
+            child: Column(
+          children: [
+            _buildLogo(context),
+            _buildSecondContainer(context),
+          ],
+        )),
       ]),
     );
   }
+
   @override
   Widget _buildLogo(BuildContext context) {
-  return Container(
-    height: 200,
-    margin: const EdgeInsets.only(top: 30),
-    child: Image.asset(
-      'assets/images/logo_without_text.png',
-    ),
-  );
+    return Container(
+      height: 200,
+      margin: const EdgeInsets.only(top: 30),
+      child: Image.asset(
+        'assets/images/logo_without_text.png',
+      ),
+    );
+  }
+
+  @override
+  Widget _buildSecondContainer(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(
+            top: 32.0, left: 32.0, right: 32.0, bottom: 56.0),
+        child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(32.0), // Adjust the padding as needed
+              child: MyForm(),
+            )),
+      ),
+    );
+  }
 }
 
-@override
-  Widget _buildSearchBar(BuildContext context) {
-    bool searchText = true;
-    return Form(
-      child: Column(
-        
-        children: [
-           TextFormField(
-            
-            obscureText: searchText,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: InputDecoration(
-              prefixIcon: IconButton(
-                onPressed: () {
-                  searchText = !searchText;
-                },
-                icon: Icon(
-                     Icons.search,
-                  color: Colors.black,
-                ),
-              ),
-              labelText: 'Search Store',
-              labelStyle: TextStyle(
-                color: Colors.grey,
-              ),
-              ),
-            ),
-        ],
-          
-      ),
-      );
+class MyForm extends StatefulWidget {
+  @override
+  _MyFormState createState() => _MyFormState();
 }
+
+class _MyFormState extends State<MyForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Search store',
+              prefixIcon: Icon(Icons.search),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
