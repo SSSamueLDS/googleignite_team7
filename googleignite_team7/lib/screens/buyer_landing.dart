a9e80ee643dd495f5738c4b8b93ef5b9c79a24e0
samuel <samuel240303@gmail.com>
2023-10-02 18:44:56 +0800

    search bar done

--- googleignite_team7/lib/screens/buyer_landing.dart
+++ googleignite_team7/lib/screens/buyer_landing.dart
@@ -7,25 +7,25 @@ class BuyerLanding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Container(
-          constraints: const BoxConstraints.expand(),
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage("assets/images/background.jpg"),
-            fit: BoxFit.cover,
-          )),
-        ),
-        SingleChildScrollView(
-            child: Column(
-          children: [
-            _buildLogo(context),
-            _buildSecondContainer(context),
-          ],
-        )),
-      ]),
+      body: Center(
+        child: Stack(children: [
+          Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/images/background.jpg"),
+              fit: BoxFit.cover,
+            )),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [_buildLogo(context), Expanded(child: SearchResults())],
+          ),
+        ]),
+      ),
     );
   }
+}
 
   @override
   Widget _buildLogo(BuildContext context) {
@@ -41,48 +41,108 @@ class BuyerLanding extends StatelessWidget {
   @override
   Widget _buildSecondContainer(BuildContext context) {
     return Container(
-      child: Padding(
-        padding: const EdgeInsets.only(
-            top: 32.0, left: 32.0, right: 32.0, bottom: 56.0),
-        child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(32.0), // Adjust the padding as needed
-              child: MyForm(),
-            )),
-      ),
+      height: 100,
+      width: 600,
+      child: 
+      SingleChildScrollView(
+        child: SearchResults(),
+      ), 
     );
   }
-}
 
-class MyForm extends StatefulWidget {
+
+class SearchResults extends StatefulWidget {
   @override
-  _MyFormState createState() => _MyFormState();
+  _SearchResultsState createState() => _SearchResultsState();
 }
 
-class _MyFormState extends State<MyForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class _SearchResultsState extends State<SearchResults> {
+  bool filteredDataVisible = false;
+
+  List<String> data = [
+    'Apple',
+    'Banana',
+    'Cherry',
+    'Date',
+    'Grapes',
+    'Lemon',
+    'Orange',
+    'Peach',
+    'Pear',
+    'Plum',
+  ];
+
+  List<String> filteredData = [];
 
   @override
   void initState() {
     super.initState();
+    
+    
+  }
+
+  void filterSearchResults(String query) {
+    List<String> dummySearchList = <String>[];
+    dummySearchList.addAll(data);
+    if (query.isNotEmpty) {
+      filteredData.addAll(data);
+      List<String> dummyListData = <String>[];
+      dummySearchList.forEach((item) {
+        if (item.toLowerCase().contains(query.toLowerCase())) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        filteredData.clear();
+        filteredData.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        filteredData.clear();
+        filteredData.addAll(data);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Search store',
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      margin: EdgeInsets.only(top: 32.0, left: 32.0, right: 32.0, bottom: 50.0),
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(32.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value) {
+              filterSearchResults(value);
+            },
+            decoration: InputDecoration(
               prefixIcon: Icon(Icons.search),
+              hintText: 'Search Store',
             ),
           ),
-        ],
+        ),
+        Container(
+          height: 400,
+          child: ListView.builder(
+            itemCount: filteredData.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text(filteredData[index]),
+                // You can add more details or customize ListTile as needed
+              );
+            },
+          ),
+        ),
+      ],
+    ),
       ),
     );
   }
