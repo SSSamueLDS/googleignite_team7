a7d2e21b90c3ff9ac72753dc74d3784f52b78c75
wang potao <37385782+bobishgr8@users.noreply.github.com>
2023-10-02 18:02:13 +0800

    return to original entry point

--- googleignite_team7/lib/main.dart
+++ googleignite_team7/lib/main.dart
@@ -124,7 +124,7 @@ Widget _buildSecondContainer(BuildContext context) {
             child: MyFilledButton(
               label: "Get Started",
               onPressed: () {
-                Navigator.pushNamed(context, '/upload');
+                Navigator.pushNamed(context, '/register_or_login');
               },
               fillColor: Colors.black,
               borderColor: Colors.transparent,
